936f22fc183de31674ce94ed4eb0456408f7055a
AkXbots <122038135+AkXbots@users.noreply.github.com>
2023-07-05 23:38:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
-OWNER_ID = int(getenv("OWNER_ID"))
+OWNER_ID = getenv("OWNER_ID")
 
 PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/5203f8fcf8cad5ae816c0.jpg")
 START_IMG = getenv("START_IMG", "https://telegra.ph/file/4bad14b030d6b434116c2.jpg")
